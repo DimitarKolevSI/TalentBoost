5aa6f1770ad50538cb3088105976cb0907f9e58b
Dimitar <141-dkolev@users.noreply.gitlab-talentboost.vmware.com>
2020-03-24 10:39:45 +0200

    First

---
+++ OOP Recap TalentBoost/src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Calculator s = new Square(5);
+	    Calculator r = new Rectangle(5,10);
+	    Calculator c = new Circle(5);
+        System.out.println(s.getArea());
+        System.out.println(r.getArea());
+        System.out.println(c.getArea());
+    }
+}
