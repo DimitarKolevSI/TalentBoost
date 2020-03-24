daf516d3527b57c87591878dbaf0b18c66965556
Dimitar <141-dkolev@users.noreply.gitlab-talentboost.vmware.com>
2020-03-24 11:59:55 +0200

    First

--- OOP Recap TalentBoost/src/com/Main.java
+++ OOP Recap TalentBoost/src/com/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println(s.getArea());
         System.out.println(r.getArea());
         System.out.println(c.getArea());
+        System.out.println(c.getArea());
         System.out.println("Done");
     }
 }
